ede3c3ac31e2071266205e39bff875fb554aa5fe
Aleksandra <a.haniok@gmail.com>
2022-06-21 14:08:02 +0000

    Add a class for retrieving a profile by id

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 # Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd party:
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -19,3 +20,24 @@ class ProfileList(APIView):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
+
+
+class ProfileDetail(APIView):
+    """
+    A class for the Profile Details
+    """
+    def get_object(self, pk):
+        """
+        Fetch the profile by id
+        """
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
+
