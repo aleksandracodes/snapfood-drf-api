00d65c7498de7bd62b055d5d4149156be0f42f04
Aleksandra <a.haniok@gmail.com>
2022-06-21 12:21:48 +0000

    Set up urls for profiles app

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Create your views here.
+# Internal:
+from .models import Profile
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class ProfileList(APIView):
+    """
+    A class view for the ProfileList
+    """
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
