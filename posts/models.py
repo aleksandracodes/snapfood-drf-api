bcb2fe725d6828de553cd7027248b2a62a225b45
Aleksandra <a.haniok@gmail.com>
2022-06-22 10:58:09 +0000

    Create Posts app and add Post model

---
+++ posts/models.py
@@ -0,0 +1,42 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.db import models
+from django.contrib.auth.models import User
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class Category(models.Model):
+    """
+    A class for the category tag model
+    """
+    category = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        """
+        Returns the category tag name string
+        """
+        return self.category
+
+
+class Post(models.Model):
+    """
+    A class for the post model
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    category = models.ManyToManyField(Category)
+    image = models.ImageField(
+        upload_to='images/',
+        default='../default_post_umaui6',
+        blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
