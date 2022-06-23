1d2f1a5d88dc7b5a4d02efe74157ca8b64899f97
Aleksandra <a.haniok@gmail.com>
2022-06-23 08:53:11 +0000

    Add a put method to PostDetail for updating the post

--- posts/views.py
+++ posts/views.py
@@ -73,3 +73,18 @@ class PostDetail(APIView):
             context={'request': request}
             )
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Update a post by id
+        """
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post,
+            data=request.data,
+            context={'request': request}
+            )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
