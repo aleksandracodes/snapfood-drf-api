a5ce0be38809f14cf16f0cc083f30108607ac2d7
Aleksandra <a.haniok@gmail.com>
2022-06-25 15:54:08 +0000

    Remove unused import

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,6 @@
 # 3rd party:
 from django.http import Http404
 from rest_framework import status
-from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
