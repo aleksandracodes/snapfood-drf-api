8a6487dc542e0e9e9065253b1932c5e69399fc0f
Aleksandra <a.haniok@gmail.com>
2022-06-25 08:11:29 +0000

    Replace regular class view with generic views for Comments

--- comments/views.py
+++ comments/views.py
@@ -1,100 +1,37 @@
 # Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 3rd party:
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Internal:
 from .models import Comment
-from .serializers import CommentSerializer
+from .serializers import CommentSerializer, CommentDetailSerializer
 from snapfood_drf_api.permissions import IsOwnerOrReadOnly
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-class CommentList(APIView):
+class CommentList(generics.ListCreateAPIView):
     """
-    A class view for the CommentList
+    A class for ClassList to view all the comments
     """
     serializer_class = CommentSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
-    ]
+        ]
+    queryset = Comment.objects.all()
 
-    def get(self, request):
-        posts = Comment.objects.all()
-        serializer = CommentSerializer(
-            posts,
-            many=True,
-            context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CommentSerializer(
-            data=request.data,
-            context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class CommentDetail(APIView):
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    A class view for comment details
-    to enable users to retrieve, update and delete the comment
+    A class for CommentDetail
+    User to be able to retrieve, edit and delete their comment
     """
-    serializer_class = CommentSerializer
-    permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        """
-        Fetch the comment by id
-        Handle the case a post does not exist
-        """
-        try:
-            post = Comment.objects.get(pk=pk)
-            self.check_object_permissions(self.request, post)
-            return post
-        except Comment.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        """
-        Retrieve a post by id
-        """
-        post = self.get_object(pk)
-        serializer = CommentSerializer(
-            post,
-            context={'request': request}
-            )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        """
-        Update a comment by id
-        """
-        post = self.get_object(pk)
-        serializer = CommentSerializer(
-            post,
-            data=request.data,
-            context={'request': request}
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        """
-        Delete a comment by id
-        """
-        post = self.get_object(pk)
-        post.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    serializer_class = CommentDetailSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+        ]
+    queryset = Comment.objects.all()
