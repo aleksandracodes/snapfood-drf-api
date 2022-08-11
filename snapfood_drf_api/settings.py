310b7f6de2f59e83fbc55539a4826587e2019f6f
Aleksandra <a.haniok@gmail.com>
2022-08-11 10:33:22 +0000

    Try: display UTC time from database in local time in front-end

--- snapfood_drf_api/settings.py
+++ snapfood_drf_api/settings.py
@@ -25,6 +25,7 @@ CLOUDINARY_STORAGE = {
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+USE_TZ = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
