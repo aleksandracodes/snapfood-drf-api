694c92456c90a6f769c58c603baa2bb5e3312a84
Aleksandra <a.haniok@gmail.com>
2022-11-21 17:09:00 +0000

    Add build.sh file for Render deployment

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py makemigrations && python manage.py migrate
-web: gunicorn snapfood_drf_api.wsgi
--- snapfood_drf_api/settings.py
+++ snapfood_drf_api/settings.py
@@ -69,6 +69,12 @@ ALLOWED_HOSTS = [
     'localhost',
 ]
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
